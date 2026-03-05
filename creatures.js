c5624b7136d4b9aa21255276b6fb70040e8607e5
Hexley <spookyglitch84@gmail.com>
2026-03-05 15:53:41 +0000

    Update creatures.js

--- creatures.js
+++ creatures.js
@@ -94,7 +94,7 @@ const creatureTypes = {
 };
 
 const creatureLocations = [
-  { id: 1,  coords: [3463, 2326], radius: 30,  creatures: ["Kokoraptor"] },
+  { id: 1,  coords: [3463, 2326], radius: 10,  creatures: ["Kokoraptor"] },
   { id: 2,  coords: [2777, 2240], radius: 50,  creatures: ["Dawnfeather"] },
   { id: 3,  coords: [2433, 2564], radius: 50,  creatures: ["Elderbraun"] },
   { id: 4,  coords: [2662, 2079], radius: 50,  creatures: ["Elderbraun"] },
@@ -103,13 +103,13 @@ const creatureLocations = [
   { id: 16, coords: [2313, 3627], radius: 50,  creatures: ["Elderbraun"] },
   { id: 7,  coords: [2192, 3170], radius: 100, creatures: ["Mocadiles"] },
   { id: 8,  coords: [2690, 988],  radius: 100, creatures: ["Quackari"] },
-  { id: 9,  coords: [3443, 2306], radius: 30,  creatures: ["Awesomesaurus"] },
+  { id: 9,  coords: [3443, 2306], radius: 10,  creatures: ["Awesomesaurus"] },
   { id: 10, coords: [2184, 1702], radius: 150, creatures: ["Sorellisk"] },
   { id: 11, coords: [2896, 2732], radius: 50,  creatures: ["Wisperhooves"] },
   { id: 12, coords: [1665, 2498], radius: 50,  creatures: ["Wisperhooves"] },
   { id: 13, coords: [1793, 2862], radius: 50,  creatures: ["Wisperhooves"] },
   { id: 14, coords: [1247, 2180], radius: 100, creatures: ["Ahria"] },
-  { id: 15, coords: [2300, 3637], radius: 100, creatures: ["Emberlyn"] },
+  { id: 15, coords: [2300, 3637], radius: 10, creatures: ["Emberlyn"] },
   { id: 17, coords: [3551, 2958], radius: 20,  creatures: ["Jasper"] },
   { id: 18, coords: [3509, 2969], radius: 20,  creatures: ["Maple"] },
   { id: 19, coords: [2838, 1466], radius: 100, creatures: ["Wimblehop"] },
